6726faa185099128ce97e4ddada82a646495b8c2
MaryGrach <arfaikot@gmail.com>
2024-11-16 11:58:10 +0300

    lab3

--- projects/digits/main.go
+++ projects/digits/main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var str string
+	fmt.Scan(&str)
+
+	rs := []rune(str)
+	var max = 0
+	for i := range rs {
+		if int(rs[i]) > max {
+			max = int(rs[i])
+		}
+	}
+	fmt.Println(string(max))
 }
--- projects/star/main.go
+++ projects/star/main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var str string
+	fmt.Scan(&str)
+	rs := []rune(str)
+	out := make([]rune, 0, 0)
+	for i := range rs {
+		if i == len(rs)-1 {
+			out = append(out, rs[i])
+		} else {
+			out = append(out, rs[i], '*')
+		}
+	}
+	fmt.Println(string(out))
 }
